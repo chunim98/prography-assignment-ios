8cd574b362de1125273dae552bc5cdefd9181225
JU98965 <jjingeo1230@naver.com>
2025-02-13 16:33:48 +0900

    init: 프로젝트 초기 설정 완료

---
+++ PrographyAssignment/Feature/Home/HomeVC.swift
@@ -0,0 +1,25 @@
+//
+//  HomeVC.swift
+//  PrographyAssignment
+//
+//  Created by 신정욱 on 2/13/25.
+//
+
+import UIKit
+
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class HomeVC: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
+#Preview {
+    HomeVC()
+}
