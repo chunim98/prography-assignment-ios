a834b844a6af5d4eeec21d98baecaf5a1fb5dcc5
JU98965 <jjingeo1230@naver.com>
2025-02-14 17:20:32 +0900

    test: 네트워킹 모듈 테스트 코드 추가

---
+++ PrographyAssignmentTests/NetworkingTests.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkingTests.swift
+//  PrographyAssignmentTests
+//
+//  Created by 신정욱 on 2/14/25.
+//
+
+import XCTest
+
+@testable import PrographyAssignment
+
+final class NetworkingTests: XCTestCase {
+
+    // 이 친구를 테스트(System Under Test)
+    var sut: NetworkManager!
+
+    override func setUpWithError() throws {
+        
+        /// 테스트 코드를 작성하기 전 기본 세팅 코드를 먼저 작성해준다.
+        /// 먼저 부모 클래스의 setUpWithError 메소드를 호출하여, 만약 에러를 포착한다면 호출자에게 에러를 전파한다.
+
+        try super.setUpWithError()
+        sut = NetworkManager.shered
+    }
+
+    override func tearDownWithError() throws {
+        
+        /// 테스트가 끝나고, 테스트에 생성된 모든 리소스나 객체를 정리.
+        /// 이렇게 하면 각 테스트가 격리되고 후속 테스트에 영향을 줄 수 있는 잔여 효과가 발생하지 않음.
+        
+        sut = nil
+        try super.tearDownWithError()
+    }
+    
+    // MARK: Testing
+    
+    func test_xcconfig_접근() {
+        let apiKey = Bundle.main.infoDictionary?["TMDB_API_KEY"] as? String ?? ""
+        print("APIKey", apiKey)
+        XCTAssertNotEqual(apiKey, "")
+    }
+    
+    func test_네트워킹() async throws {
+        let nowPlaying = try? await sut.fetchNowPlaying()
+        nowPlaying?.results.forEach { print($0) }
+        
+        XCTAssertNotNil(nowPlaying, "nil인듯")
+    }
+
+}
