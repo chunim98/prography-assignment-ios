941f8d2fd9f708bf111b9135ced8e3cbceba5dc6
JU98965 <jjingeo1230@naver.com>
2025-02-17 18:44:57 +0900

    test: 영화의 세부정보 받아오기 테스트 케이스 추가

--- PrographyAssignmentTests/NetworkingTests.swift
+++ PrographyAssignmentTests/NetworkingTests.swift
@@ -60,4 +60,17 @@ final class NetworkingTests: XCTestCase {
         
         XCTAssertNotNil(data, "nil인듯")
     }
+    
+    func test_영화_세부정보_받아오기() async throws {
+        let data = try await sut.fetchMovieDetails(939243) // "수퍼 소닉 3"의 id를 사용
+        
+        print(data.genres)
+        print(data.id)
+        print(data.overview)
+        print(data.posterPath)
+        print(data.title)
+        print(data.voteAverage)
+        
+        XCTAssertNotNil(data, "nil인듯")
+    }
 }
