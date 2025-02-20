99bea0f47dfd811fbb127ce76ccefd7149430d6c
JU98965 <jjingeo1230@naver.com>
2025-02-21 06:56:32 +0900

    feat: 런치 스크린, 앱 아이콘 추가

--- PrographyAssignment/Common/Application/AppDelegate.swift
+++ PrographyAssignment/Common/Application/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 런치 화면 딜레이 설정
+        Thread.sleep(forTimeInterval: 0.75)
         return true
     }
 
